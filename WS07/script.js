8ba1a925b1c9c5facedcc0bdb1a962465bb6866f
Kimpale1 <KIK00139@laurea.fi>
2026-05-08 16:48:21 +0300

    etenee

--- WS07/script.js
+++ WS07/script.js
@@ -1,18 +1,25 @@
 function silitaTopoa() {
+  const btn = document.getElementById("pet-btn");
   const message = document.getElementById("pet-message");
   const img = document.getElementById("topo-react");
   const sound = document.getElementById("purr-sound");
 
+  // 1. piilota nappi
+  btn.style.display = "none";
+
+  // 2. näytä reaktio
   message.innerText = "Töpö kehrää tyytyväisenä 😌🐾";
-
   img.classList.remove("d-none");
- 
-  setTimeout(() => {
-    img.classList.remove("d-none");
-  }, 200);
 
+  // 3. ääni
   sound.currentTime = 0;
   sound.play();
 
+  // 4. reset 3 sekunnin päästä
+  setTimeout(() => {
+    img.classList.add("d-none");
+    message.innerText = "";
+    btn.style.display = "inline-block";
+  }, 13000);
 }
 
